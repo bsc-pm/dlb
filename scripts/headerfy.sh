265f38372aab4fedb3d7eaf4cd33dddd3385af3d
Victor Lopez <victor.lopez@bsc.es>
2014-10-31 14:24:21 +0100

    Update headers and add script to automatize the process

---
+++ scripts/headerfy.sh
@@ -0,0 +1,6 @@
+#Run this script from the base directory
+
+find . -name "*.c" -exec headache -h scripts/preamble.txt -c scripts/headache.cfg {} +
+find . -name "*.h" -exec headache -h scripts/preamble.txt -c scripts/headache.cfg {} +
+find . -name "Makefile.am" -exec headache -h scripts/preamble.txt -c scripts/headache.cfg {} +
+
