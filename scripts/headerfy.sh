2901b889053c5dff772a1255b706097bbea492a1
Victor Lopez <victor.lopez@bsc.es>
2015-07-29 17:20:57 +0200

    Update license header

--- scripts/headerfy.sh
+++ scripts/headerfy.sh
@@ -2,5 +2,7 @@
 
 find . -name "*.c" -exec headache -h scripts/preamble.txt -c scripts/headache.cfg {} +
 find . -name "*.h" -exec headache -h scripts/preamble.txt -c scripts/headache.cfg {} +
+find . -name "*.c.in" -exec headache -h scripts/preamble.txt -c scripts/headache.cfg {} +
+find . -name "*.h.in" -exec headache -h scripts/preamble.txt -c scripts/headache.cfg {} +
 find . -name "Makefile.am" -exec headache -h scripts/preamble.txt -c scripts/headache.cfg {} +
 
