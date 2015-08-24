1eaec85f6db0ff5e176ada5d1201f2e9c2ea5225
Victor Lopez <victor.lopez@bsc.es>
2015-08-24 15:36:22 +0200

    Add list/remove functionalities to dlb_taskset

--- src/utils/dlb_taskset.c
+++ src/utils/dlb_taskset.c
@@ -32,11 +32,9 @@
 #include <stdbool.h>
 #include "LB_core/DLB_interface.h"
 
-// The next two functions are a copy/paste of the ones defined in the mask_utils
-// module. Due to symbol visibility they are replicated here, although a better
-// solution that considered better maintainability would be preferred
-static const char* mu_to_str ( const cpu_set_t *cpu_set ) {
-    int sys_size = DLB_Drom_GetNumCpus();
+static int sys_size;
+
+static const char* mu_to_str( const cpu_set_t *cpu_set ) {
     int i;
     static char str[CPU_SETSIZE*4];
     char str_i[8];
@@ -51,8 +49,7 @@ static const char* mu_to_str ( const cpu_set_t *cpu_set ) {
     return str;
 }
 
-static void mu_parse_mask( char *str, cpu_set_t *mask ) {
-    int sys_size = DLB_Drom_GetNumCpus();
+static void mu_parse_mask( const char *str, cpu_set_t *mask ) {
     regex_t regex_bitmask;
     regex_t regex_range;
     CPU_ZERO( mask );
@@ -79,7 +76,7 @@ static void mu_parse_mask( char *str, cpu_set_t *mask ) {
     /* Regular expression matches range, e.g.: 0-3,6-7 */
     else if ( !regexec(&regex_range, str, 0, NULL, 0) ) {
         // Parse
-        char *ptr = str;
+        const char *ptr = str;
         char *endptr;
         while ( ptr < str+strlen(str) ) {
             // Discard junk at the left
@@ -117,40 +114,101 @@ static void mu_parse_mask( char *str, cpu_set_t *mask ) {
 
     regfree(&regex_bitmask);
     regfree(&regex_range);
+
+    if ( CPU_COUNT(mask) == 0 ) {
+        fprintf( stderr, "Parsed mask does not seem to be a valid mask\n" );
+        exit( EXIT_FAILURE );
+    }
+}
+
+static void dlb_check( int error, pid_t pid ) {
+    if ( error ) {
+        if ( pid ) {
+            fprintf( stderr, "Could not find PID %d in DLB shared memory\n", pid );
+        } else {
+            fprintf( stderr, "Unknown error calling DLB API\n" );
+        }
+        DLB_Drom_Finalize();
+        exit( EXIT_FAILURE );
+    }
 }
 
 static void print_usage( const char * program ) {
-    fprintf( stdout, "usage: %s -p PID [-c CPU_LIST]\n", program );
-    fprintf( stdout, "Try '%s --help' for more information.\n", program );
+    fprintf( stdout, "usage:\n" );
+    fprintf( stdout, "\t%s [-l|--list] [-p <pid>]\n", program );
+    fprintf( stdout, "\t%s [-s|--set <cpu_list> -p <pid>]\n", program );
+    fprintf( stdout, "\t%s [-r|--remove <cpu_list>] [-p <pid>]\n", program );
 }
 
 static void print_help( const char * program ) {
     fprintf( stdout, "DLB - Dynamic Load Balancing, version %s.\n", VERSION );
-    fprintf( stdout, "usage: %s -p PID [-c CPU_LIST]\n", program );
+    print_usage(program);
     fprintf( stdout, "\n" );
-    fprintf( stdout, "Show or change the CPU affinity of a DLB process.\n" );
+    fprintf( stdout, "Retrieve or modify the CPU affinity of DLB processes.\n" );
     fprintf( stdout, "\n" );
     fprintf( stdout, "Options:\n" );
-    fprintf( stdout, "  -p, --pid       operate on existing given pid\n" );
-    fprintf( stdout, "  -c, --cpu-list  change affinity according to cpu list (e.g., -c 0,5-7)\n" );
-    fprintf( stdout, "  -h, --help      print this help\n" );
+    fprintf( stdout, "  -l, --list                  list all DLB processes and their masks\n" );
+    fprintf( stdout, "  -s, --set <cpu_list>        set affinity according to cpu_list (e.g., -c 0,5-7)\n" );
+    fprintf( stdout, "  -r, --remove <cpu_list>     remove CPU ownership of any DLB process according to cpu_list\n" );
+    fprintf( stdout, "  -p, --pid                   operate only on existing given pid\n" );
+    fprintf( stdout, "  -h, --help                  print this help\n" );
 }
 
-
 static void set_affinity( pid_t pid, char *cpu_list ) {
-    int error;
     cpu_set_t new_mask;
     mu_parse_mask( cpu_list, &new_mask );
 
     DLB_Drom_Init();
-    error = DLB_Drom_SetProcessMask( pid, &new_mask );
+    int error = DLB_Drom_SetProcessMask( pid, &new_mask );
+    dlb_check( error, pid );
     DLB_Drom_Finalize();
 
-    if ( error ) {
-        fprintf( stderr, "Could not find PID %d in DLB shared memory\n", pid );
-    } else {
-        fprintf( stdout, "PID %d's current affinity set to: %s\n", pid, mu_to_str(&new_mask) );
+    fprintf( stdout, "PID %d's current affinity set to: %s\n", pid, mu_to_str(&new_mask) );
+}
+
+static void remove_affinity_of_one( pid_t pid, const cpu_set_t *cpus_to_remove ) {
+    int i, error;
+
+    // Get current PID's mask
+    cpu_set_t pid_mask;
+    error = DLB_Drom_GetProcessMask(pid, &pid_mask);
+    dlb_check( error, pid );
+
+    // Remove cpus from the PID's mask
+    for ( i=0; i<sys_size; ++i ) {
+        if ( CPU_ISSET(i, cpus_to_remove) ) {
+            CPU_CLR( i, &pid_mask );
+        }
     }
+
+    // Apply final mask
+    error = DLB_Drom_SetProcessMask(pid, &pid_mask);
+    dlb_check( error, pid );
+}
+
+static void remove_affinity( pid_t pid, const char *cpu_list ) {
+    cpu_set_t cpus_to_remove;
+    mu_parse_mask( cpu_list, &cpus_to_remove );
+
+    DLB_Drom_Init();
+
+    if ( pid ) {
+        remove_affinity_of_one( pid, &cpus_to_remove );
+    }
+    else {
+        // Get PID list from DLB
+        int nelems;
+        int *pidlist = (int*) malloc(sys_size*sizeof(int));
+        DLB_Drom_GetPidList( pidlist, &nelems, sys_size);
+
+        // Iterate pidlist
+        int i;
+        for ( i=0; i<nelems; ++i) {
+            remove_affinity_of_one( pidlist[i], &cpus_to_remove );
+        }
+        free(pidlist);
+    }
+    DLB_Drom_Finalize();
 }
 
 static void show_affinity( pid_t pid ) {
@@ -158,19 +216,24 @@ static void show_affinity( pid_t pid ) {
     cpu_set_t mask;
 
     DLB_Drom_Init();
-    error = DLB_Drom_GetProcessMask( pid, &mask );
-    DLB_Drom_Finalize();
-
-    if ( error ) {
-        fprintf( stderr, "Could not find PID %d in DLB shared memory\n", pid );
-    } else {
+    if ( pid ) {
+        // Show CPU affinity of given PID
+        error = DLB_Drom_GetProcessMask( pid, &mask );
+        dlb_check( error, pid );
         fprintf( stdout, "PID %d's current affinity CPU list: %s\n", pid, mu_to_str(&mask) );
+    } else {
+        // Show CPU affinity of all processes attached to DLB
+        DLB_Drom_PrintShmem();
     }
+    DLB_Drom_Finalize();
 }
 
 
 int main ( int argc, char *argv[] ) {
+    bool do_list = false;
     bool do_help = false;
+    bool do_set = false;
+    bool do_remove = false;
 
     pid_t pid = 0;
     char *cpu_list = NULL;
@@ -178,23 +241,34 @@ int main ( int argc, char *argv[] ) {
     int opt;
     extern char *optarg;
     struct option long_options[] = {
+        {"list",     no_argument,       0, 'l'},
+        {"set",      required_argument, 0, 's'},
+        {"remove",   required_argument, 0, 'r'},
         {"pid",      required_argument, 0, 'p'},
-        {"cpu-list", required_argument, 0, 'c'},
         {"help",     no_argument,       0, 'h'},
         {0,          0,                 0, 0 }
     };
 
-    while ( (opt = getopt_long(argc, argv, "p:c:h", long_options, NULL)) != -1 ) {
+    while ( (opt = getopt_long(argc, argv, "ls:r:p:h", long_options, NULL)) != -1 ) {
         switch (opt) {
-        case 'h':
-            do_help = true;
+        case 'l':
+            do_list = true;
+            break;
+        case 's':
+            do_set = true;
+            cpu_list = (char*) malloc( strlen(optarg) );
+            strcpy( cpu_list, optarg );
+            break;
+        case 'r':
+            do_remove = true;
+            cpu_list = (char*) malloc( strlen(optarg) );
+            strcpy( cpu_list, optarg );
             break;
         case 'p':
             pid = strtoul( optarg, NULL, 10 );
             break;
-        case 'c':
-            cpu_list = (char*) malloc( strlen(optarg) );
-            strcpy( cpu_list, optarg );
+        case 'h':
+            do_help = true;
             break;
         default:
             print_usage( argv[0] );
@@ -202,22 +276,39 @@ int main ( int argc, char *argv[] ) {
         }
     }
 
-    if ( !do_help && !pid ) {
+    // Argument checks
+    if ( do_set && do_remove ) {
+        fprintf(stderr, "Only one option --set/--remove allowed\n");
         print_usage( argv[0] );
-        exit( EXIT_SUCCESS );
+        exit( EXIT_FAILURE );
     }
 
+    if ( do_set && !pid ) {
+        fprintf(stderr, "Option --set requires a pid\n");
+        print_usage( argv[0] );
+        exit( EXIT_FAILURE );
+    }
+
+    sys_size = DLB_Drom_GetNumCpus();
+
+    // Actions
     if ( do_help ) {
-        print_help(argv[0] );
-        exit( EXIT_SUCCESS );
+        print_help(argv[0]);
     }
-
-    if ( cpu_list ) {
+    else if ( do_set ) {
         set_affinity( pid, cpu_list );
-    } else {
+    }
+    else if ( do_remove ) {
+        remove_affinity( pid, cpu_list );
+    }
+    else if ( do_list || pid ) {
         show_affinity( pid );
+    }
+    else {
+        print_usage( argv[0] );
+        fprintf( stdout, "Try '%s --help' for more information.\n", argv[0] );
     }
 
     free( cpu_list );
-    return EXIT_SUCCESS;;
+    return EXIT_SUCCESS;
 }
