5d1c60e9329320420cde9b0a59737ea7b0dcb373
Victor Lopez <victor.lopez@bsc.es>
2016-12-20 11:29:32 +0100

    Add dlb .ico file

--- doc/user_guide/source/conf.py
+++ doc/user_guide/source/conf.py
@@ -137,7 +137,7 @@ html_logo = 'images/dlb_logo_white.png'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'images/dlb_icon.png'
+html_favicon = 'images/dlb_icon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
