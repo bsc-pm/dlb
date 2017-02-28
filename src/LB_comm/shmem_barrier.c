87800f4d93d33107af9196df6cee042c942a08b9
Marta Garcia <marta.garcia@bsc.es>
2017-02-28 09:59:15 +0100

    Do not use LB_Barrier_QTY var, the number of processes that are in the barrier are all the ones that haver done init with LB_Barrier=1

--- src/LB_comm/shmem_barrier.c
+++ src/LB_comm/shmem_barrier.c
@@ -64,7 +64,7 @@ void shmem_barrier_init(void) {
     // more error checks to be done if the option is implemented
     global_barrier_ids[0] = getenv("LB_BARRIER_ID") ? atoi(getenv("LB_BARRIER_ID")) : 0;
     global_barrier_ids[1] = global_barrier_ids[0]+1;
-    int qty = getenv("LB_BARRIER_QTY") ? atoi(getenv("LB_BARRIER_QTY")) : 0;
+//    int qty = getenv("LB_BARRIER_QTY") ? atoi(getenv("LB_BARRIER_QTY")) : 0;
 
     max_barriers = mu_get_system_size()*2;
 
@@ -86,9 +86,11 @@ void shmem_barrier_init(void) {
                 }
 
                 barrier->count = 0;
-                barrier->participants = qty;
+                barrier->participants = 0;
                 barrier->initialized = true;
             }
+            barrier->participants++;
+            warning("barrier participants: %d", barrier->participants)
             advance_barrier();
         }
     }
