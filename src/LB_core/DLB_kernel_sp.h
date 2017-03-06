22ad43c42aa8f8de93588e9f3c7df95f2697bc85
Victor Lopez <victor.lopez@bsc.es>
2017-03-06 15:29:54 +0100

    Fix typo in parameter name

--- src/LB_core/DLB_kernel_sp.h
+++ src/LB_core/DLB_kernel_sp.h
@@ -32,42 +32,42 @@
 /* Status */
 subprocess_descriptor_t* Initialize_sp(int ncpus, const cpu_set_t *mask, const char *lb_args);
 int Finish_sp(subprocess_descriptor_t *spd);
-int set_dlb_enabled_sp(subprocess_descriptor_t *psd, bool enabled);
-int set_max_parallelism_sp(subprocess_descriptor_t *psd, int max);
+int set_dlb_enabled_sp(subprocess_descriptor_t *spd, bool enabled);
+int set_max_parallelism_sp(subprocess_descriptor_t *spd, int max);
 
 /* Callbacks */
-int callback_set_sp(subprocess_descriptor_t *psd, dlb_callbacks_t which, dlb_callback_t callback);
-int callback_get_sp(subprocess_descriptor_t *psd, dlb_callbacks_t which, dlb_callback_t *callback);
+int callback_set_sp(subprocess_descriptor_t *spd, dlb_callbacks_t which, dlb_callback_t callback);
+int callback_get_sp(subprocess_descriptor_t *spd, dlb_callbacks_t which, dlb_callback_t *callback);
 
 /* Lend */
-int lend_sp(subprocess_descriptor_t *psd);
-int lend_cpu_sp(subprocess_descriptor_t *psd, int cpuid);
-int lend_cpus_sp(subprocess_descriptor_t *psd, int ncpus);
-int lend_cpu_mask_sp(subprocess_descriptor_t *psd, const cpu_set_t *mask);
+int lend_sp(subprocess_descriptor_t *spd);
+int lend_cpu_sp(subprocess_descriptor_t *spd, int cpuid);
+int lend_cpus_sp(subprocess_descriptor_t *spd, int ncpus);
+int lend_cpu_mask_sp(subprocess_descriptor_t *spd, const cpu_set_t *mask);
 
 /* Reclaim */
-int reclaim_sp(subprocess_descriptor_t *psd);
-int reclaim_cpu_sp(subprocess_descriptor_t *psd, int cpuid);
-int reclaim_cpus_sp(subprocess_descriptor_t *psd, int ncpus);
-int reclaim_cpu_mask_sp(subprocess_descriptor_t *psd, const cpu_set_t *mask);
+int reclaim_sp(subprocess_descriptor_t *spd);
+int reclaim_cpu_sp(subprocess_descriptor_t *spd, int cpuid);
+int reclaim_cpus_sp(subprocess_descriptor_t *spd, int ncpus);
+int reclaim_cpu_mask_sp(subprocess_descriptor_t *spd, const cpu_set_t *mask);
 
 /* Acquire */
-int acquire_sp(subprocess_descriptor_t *psd);
-int acquire_cpu_sp(subprocess_descriptor_t *psd, int cpuid);
-int acquire_cpus_sp(subprocess_descriptor_t *psd, int ncpus);
-int acquire_cpu_mask_sp(subprocess_descriptor_t *psd, const cpu_set_t *mask);
+int acquire_sp(subprocess_descriptor_t *spd);
+int acquire_cpu_sp(subprocess_descriptor_t *spd, int cpuid);
+int acquire_cpus_sp(subprocess_descriptor_t *spd, int ncpus);
+int acquire_cpu_mask_sp(subprocess_descriptor_t *spd, const cpu_set_t *mask);
 
 /* Return */
-int return_all_sp(subprocess_descriptor_t *psd);
-int return_cpu_sp(subprocess_descriptor_t *psd, int cpuid);
-int return_cpu_mask_sp(subprocess_descriptor_t *psd, const cpu_set_t *mask);
+int return_all_sp(subprocess_descriptor_t *spd);
+int return_cpu_sp(subprocess_descriptor_t *spd, int cpuid);
+int return_cpu_mask_sp(subprocess_descriptor_t *spd, const cpu_set_t *mask);
 
 /* DROM Responsive */
-int poll_drom_sp(subprocess_descriptor_t *psd, int *new_cpus, cpu_set_t *new_mask);
+int poll_drom_sp(subprocess_descriptor_t *spd, int *new_cpus, cpu_set_t *new_mask);
 
 /* Misc */
-int set_variable_sp(subprocess_descriptor_t *psd, const char *variable, const char *value);
-int get_variable_sp(subprocess_descriptor_t *psd, const char *variable, char *value);
-int print_variables_sp(subprocess_descriptor_t *psd);
+int set_variable_sp(subprocess_descriptor_t *spd, const char *variable, const char *value);
+int get_variable_sp(subprocess_descriptor_t *spd, const char *variable, char *value);
+int print_variables_sp(subprocess_descriptor_t *spd);
 
 #endif /* DLB_KERNEL_SP_H */
