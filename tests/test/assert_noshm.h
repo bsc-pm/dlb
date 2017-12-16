c4b2419f3e603fd5dcac1988a2e3fc80efecdcb6
Victor Lopez <victor.lopez@bsc.es>
2017-12-16 18:13:16 +0100

    Add lewi shmem check to tests

--- tests/test/assert_noshm.h
+++ tests/test/assert_noshm.h
@@ -11,25 +11,27 @@
  *  * Abort if any shared memory file exists upon program finalization
  */
 enum { SHMEM_MAX_NAME_LENGTH = 64 };
+static const char* const shmem_names[] = {"lewi", "cpuinfo", "procinfo", "async"};
+enum { shmem_names_nelems = sizeof(shmem_names) / sizeof(shmem_names[0]) };
 
 __attribute__((constructor)) static void delete_shm(void) {
     char shm_filename[SHMEM_MAX_NAME_LENGTH];
-    snprintf(shm_filename, SHMEM_MAX_NAME_LENGTH, "/dev/shm/DLB_cpuinfo_%d", getuid());
-    unlink(shm_filename);
-    snprintf(shm_filename, SHMEM_MAX_NAME_LENGTH, "/dev/shm/DLB_procinfo_%d", getuid());
-    unlink(shm_filename);
-    snprintf(shm_filename, SHMEM_MAX_NAME_LENGTH, "/dev/shm/DLB_async_%d", getuid());
-    unlink(shm_filename);
+    int i;
+    for (i=0; i<shmem_names_nelems; ++i) {
+        snprintf(shm_filename, SHMEM_MAX_NAME_LENGTH,
+                "/dev/shm/DLB_%s_%d", shmem_names[i], getuid());
+        unlink(shm_filename);
+    }
 }
 
 __attribute__((destructor)) static void assert_noshm(void) {
     char shm_filename[SHMEM_MAX_NAME_LENGTH];
-    snprintf(shm_filename, SHMEM_MAX_NAME_LENGTH, "/dev/shm/DLB_cpuinfo_%d", getuid());
-    assert( access(shm_filename, F_OK) == -1 );
-    snprintf(shm_filename, SHMEM_MAX_NAME_LENGTH, "/dev/shm/DLB_procinfo_%d", getuid());
-    assert( access(shm_filename, F_OK) == -1 );
-    snprintf(shm_filename, SHMEM_MAX_NAME_LENGTH, "/dev/shm/DLB_async_%d", getuid());
-    assert( access(shm_filename, F_OK) == -1 );
+    int i;
+    for (i=0; i<shmem_names_nelems; ++i) {
+        snprintf(shm_filename, SHMEM_MAX_NAME_LENGTH,
+                "/dev/shm/DLB_%s_%d", shmem_names[i], getuid());
+        assert( access(shm_filename, F_OK) == -1 );
+    }
 }
 
 #endif /* ASSERT_NOSHM_H */
