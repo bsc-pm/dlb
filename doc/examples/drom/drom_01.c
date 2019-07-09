5f6cc322fa6fbdfb1a7029663d2c224874484147
Victor Lopez <victor.lopez@bsc.es>
2019-07-09 12:38:10 +0200

    Improve examples READMEs and scripts
    * DROM example now supports 2+ processes
    * Added --lewi-mpi for all three LeWI examples

--- doc/examples/drom/drom_01.c
+++ doc/examples/drom/drom_01.c
@@ -32,7 +32,7 @@ static void sighandler(int signum)
     }
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     struct sigaction sa;
     sa.sa_handler = &sighandler;
@@ -44,21 +44,35 @@ int main()
            "Press 'Ctrl-C' to gracefully stop the execution and clean DLB shared memories.\n"
            "PID: %d\n", getpid());
 
-    DLB_Init(0, NULL, "--drom");
+    int err = DLB_Init(0, NULL, "--drom");
+    if (err == DLB_SUCCESS) {
 
-    int num_threads = omp_get_max_threads();
-    printf("Starting example with number of threads: %d\n", num_threads);
+        int num_threads = omp_get_max_threads();
+        printf("Starting example with number of threads: %d\n", num_threads);
 
-    while(run == 1) {
-        if (DLB_PollDROM(&num_threads, NULL) == DLB_SUCCESS) {
-            printf("Number of threads changed to %d\n", num_threads);
+        while(run == 1) {
+            if (DLB_PollDROM(&num_threads, NULL) == DLB_SUCCESS) {
+                if (num_threads > 0) {
+                    printf("Number of threads changed to %d\n", num_threads);
+                } else if (num_threads == 0) {
+                    printf("WARNING: Assigned CPUs changed to 0.\n");
+                }
+            }
+
+            #pragma omp parallel num_threads(num_threads)
+            {
+                usleep(500000);
+            }
+            __sync_synchronize();
         }
-
-        #pragma omp parallel num_threads(num_threads)
-        {
-            usleep(500000);
-        }
-        __sync_synchronize();
+    } else if (err == DLB_ERR_PERM) {
+        printf("DLB_Init error:\n"
+                "    There are no CPU slots left for this process.\n"
+                "    Remember to remove some CPUs first from any running process with dlb_taskset\n"
+                "    and then run this process again with taskset -c [cpu_list] %s.\n"
+                    , argv[0]);
+    } else {
+        printf("DLB failed with the following error: %s\n", DLB_Strerror(err));
     }
 
     printf("Finalizing DROM example.\n");
