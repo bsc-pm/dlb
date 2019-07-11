fa12f22ea6a197a35f95ed2a08dcee45e9cad61b
Victor Lopez <victor.lopez@bsc.es>
2019-07-11 17:39:13 +0200

    Add better error descriptions

--- src/support/error.c
+++ src/support/error.c
@@ -22,24 +22,24 @@
 #include "apis/dlb_errors.h"
 
 static const char* error_msg[] = {
-    /* DLB_NOUPDT */        "no update needed",
-    /* DLB_NOTED */         "resource not available, but petition attended",
+    /* DLB_NOUPDT */        "The requested operation does not need any action",
+    /* DLB_NOTED */         "The operation cannot be performed now, but it has been attended",
     /* DLB_SUCCESS */       "Success",
     /* DLB_ERR_UNKNOWN */   "Unknown error",
-    /* DLB_ERR_NOINIT */    "DLB is not initialized",
-    /* DLB_ERR_INIT */      "DLB already initialized",
+    /* DLB_ERR_NOINIT */    "DLB has not been initialized",
+    /* DLB_ERR_INIT */      "DLB is already initialized",
     /* DLB_ERR_DISBLD */    "DLB is disabled",
-    /* DLB_ERR_NOSHMEM */   "No shared memory",
-    /* DLB_ERR_NOPROC */    "No pid",
-    /* DLB_ERR_PDIRTY */    "pid dirty, can't update",
-    /* DLB_ERR_PERM */      "permission error",
-    /* DLB_ERR_TIMEOUT */   "timeout",
-    /* DLB_ERR_NOCBK */     "no callback defined",
-    /* DLB_ERR_NOENT */     "no entry",
-    /* DLB_ERR_NOCOMP */    "no compatible",
-    /* DLB_ERR_REQST */     "too many requests",
-    /* DLB_ERR_NOMEM */     "not enough space",
-    /* DLB_ERR_NOPOL */     "not defined in current policy"
+    /* DLB_ERR_NOSHMEM */   "DLB cannot find a shared memory",
+    /* DLB_ERR_NOPROC */    "DLB cannot find the requested process",
+    /* DLB_ERR_PDIRTY */    "DLB cannot update the target process, operation still in process",
+    /* DLB_ERR_PERM */      "DLB cannot acquire the requested resource",
+    /* DLB_ERR_TIMEOUT */   "The operation has timed out",
+    /* DLB_ERR_NOCBK */     "The callback is not defined and cannot be invoked",
+    /* DLB_ERR_NOENT */     "The queried entry does not exist",
+    /* DLB_ERR_NOCOMP */    "The operation is not compatible with the configured DLB options",
+    /* DLB_ERR_REQST */     "DLB cannot take more requests for a specific resource",
+    /* DLB_ERR_NOMEM */     "DLB cannot allocate more processes into the shared memory",
+    /* DLB_ERR_NOPOL */     "The operation is not defined in the current policy"
 };
 
 const char* error_get_str(int errnum) {
