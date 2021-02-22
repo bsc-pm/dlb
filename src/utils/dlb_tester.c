e1e257cb51b35e4f305e92727f984b714a88554a
Victor Lopez <victor.lopez@bsc.es>
2021-02-22 12:18:59 +0100

    Implement LeWI capabilities in dlb_tester

--- src/utils/dlb_tester.c
+++ src/utils/dlb_tester.c
@@ -27,12 +27,15 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <signal.h>
+#include <string.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <limits.h>
 #include <time.h>
 #include <omp.h>
 
 #include "apis/dlb.h"
+#include "support/mask_utils.h"
 
 enum { QUERY_INTERVAL_DEFAULT_MS = 1000 };
 
@@ -43,10 +46,10 @@ static void sighandler(int signum) {
     keep_running = 0;
 }
 
-/* Return timespec diff in ms */
-static int timespec_diff(const struct timespec *init, const struct timespec *end) {
-    return (int)(end->tv_sec - init->tv_sec) * 1000L +
-        (int)(end->tv_nsec - init->tv_nsec) / 1000000L;
+static int64_t get_time_in_ms(void) {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_nsec / 1000000L + (int64_t)ts.tv_sec * 1000L;
 }
 
 static void __attribute__((__noreturn__)) usage(const char *program, FILE *out) {
@@ -56,9 +59,11 @@ static void __attribute__((__noreturn__)) usage(const char *program, FILE *out) 
     fprintf(out, "usage: %s [OPTIONS]\n", program);
     fputs((
                 "Options:\n"
-                "  -b, --busy-wait          force 100%% CPU usage while waiting\n"
-                "  -i, --interval <ms>      amount of milliseconds between each update\n"
-                "  -h, --help               print this help\n"
+                "  -b, --busy-wait              force 100%% CPU usage while waiting\n"
+                "  -i, --interval <ms>          amount of milliseconds between each update\n"
+                "  -l, --lend-cpus[=<cpuset>]   lend CPUs to LeWI, defaults to all\n"
+                "      --lend-interval <ms>     if specified, interval to reclaim lent CPUs\n"
+                "  -h, --help                   print this help\n"
                 "\n"
                 ), out);
 
@@ -66,6 +71,10 @@ static void __attribute__((__noreturn__)) usage(const char *program, FILE *out) 
 }
 
 int main(int argc, char *argv[]) {
+    /* Obtain process mask */
+    cpu_set_t process_mask;
+    sched_getaffinity(0, sizeof(process_mask), &process_mask);
+
     /* Register new actions for SIGINT and SIGTERM */
     struct sigaction new_action;
     new_action.sa_handler = sighandler;
@@ -79,18 +88,28 @@ int main(int argc, char *argv[]) {
         perror("Error: cannot handle SIGINT");
     }
 
+    /* Long options that have no corresponding short option */
+    enum {
+        LEND_INTERVAL_OPTION = CHAR_MAX + 1
+    };
+
     /* Parse options */
     bool busy_wait = false;
     int query_interval_ms = QUERY_INTERVAL_DEFAULT_MS;
+    bool lewi = false;
+    cpu_set_t cpus_to_lend;
+    int lend_cpus_interval = 0;
     int opt;
     extern char *optarg;
     struct option long_options[] = {
-        {"busy-wait", no_argument,       NULL, 'b'},
-        {"interval",  required_argument, NULL, 'i'},
-        {"help",      no_argument,       NULL, 'h'},
-        {0,           0,                 NULL, 0 }
+        {"busy-wait",     no_argument,       NULL, 'b'},
+        {"interval",      required_argument, NULL, 'i'},
+        {"lend-cpus",     optional_argument, NULL, 'l'},
+        {"lend-interval", required_argument, NULL, LEND_INTERVAL_OPTION},
+        {"help",          no_argument,       NULL, 'h'},
+        {0,               0,                 NULL, 0 }
     };
-    while ((opt = getopt_long(argc, argv, "bi:h", long_options, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "bi:l::h", long_options, NULL)) != -1) {
         switch (opt) {
             case 'b':
                 busy_wait = true;
@@ -99,6 +118,18 @@ int main(int argc, char *argv[]) {
                 query_interval_ms = strtol(optarg, NULL, 0);
                 if (!query_interval_ms)
                     query_interval_ms = QUERY_INTERVAL_DEFAULT_MS;
+                break;
+            case 'l':
+                lewi = true;
+                if (optarg) {
+                    printf("optarg: %s\n", optarg);
+                    mu_parse_mask(optarg, &cpus_to_lend);
+                } else {
+                    memcpy(&cpus_to_lend, &process_mask, sizeof(process_mask));
+                }
+                break;
+            case LEND_INTERVAL_OPTION:
+                lend_cpus_interval = strtol(optarg, NULL, 0);
                 break;
             case 'h':
                 usage(argv[0], stdout);
@@ -109,26 +140,30 @@ int main(int argc, char *argv[]) {
         }
     }
     printf("Query interval: %d ms\n", query_interval_ms);
+    if (lend_cpus_interval > 0) {
+        printf("LeWI interval:  %d ms\n", lend_cpus_interval);
+    }
 
     /* Initialize DLB */
-    cpu_set_t mask;
-    sched_getaffinity(0, sizeof(mask), &mask);
-    DLB_Init(0, &mask, "--drom");
+    DLB_Init(0, &process_mask, "--drom --lewi");
     int system_size = sysconf(_SC_NPROCESSORS_ONLN);
     int *bindings = malloc(sizeof(int)*system_size);
+
+    /* Lend CPUs to LeWI on first iteration only if interval is not specified */
+    bool lewi_lend = lend_cpus_interval == 0;
+    bool lewi_reclaim = false;
+    bool lewi_has_lent = false;
+    int64_t lewi_timestamp = get_time_in_ms();
 
     while (keep_running) {
         /* Parallel region of aprox query_interval_ms long */
         #pragma omp parallel
         {
             if (busy_wait) {
-                struct timespec start_loop;
-                clock_gettime(CLOCK_MONOTONIC, &start_loop);
-                int interval = 0;
+                int64_t start_loop = get_time_in_ms();
+                int64_t interval = 0;
                 do {
-                    struct timespec now;
-                    clock_gettime(CLOCK_MONOTONIC, &now);
-                    interval = timespec_diff(&start_loop, &now);
+                    interval = get_time_in_ms() - start_loop;
                 } while(interval < query_interval_ms);
             } else {
                 usleep(query_interval_ms*1000L);
@@ -137,10 +172,47 @@ int main(int argc, char *argv[]) {
 
         /* Poll DLB */
         int new_threads;
-        int err = DLB_PollDROM(&new_threads, NULL);
+        int err = DLB_PollDROM(&new_threads, &process_mask);
         if (err == DLB_SUCCESS) {
             printf("\nChanging to %d threads\n", new_threads);
             omp_set_num_threads(new_threads);
+            lewi_lend = true;
+        }
+
+        /* LeWI */
+        if (lewi) {
+            // Compute lend cpus interval
+            if (lend_cpus_interval > 0) {
+                int64_t now = get_time_in_ms();
+                if (now - lewi_timestamp > lend_cpus_interval) {
+                    lewi_timestamp = now;
+                    lewi_lend = !lewi_has_lent;
+                    lewi_reclaim = lewi_has_lent;
+                    lewi_has_lent = !lewi_has_lent;
+                }
+            }
+
+            // Lend
+            if (lewi_lend) {
+                printf("\nLending mask %s\n", mu_to_str(&cpus_to_lend));
+                // create mask and call DLB
+                cpu_set_t mask;
+                CPU_AND(&mask, &process_mask, &cpus_to_lend);
+                DLB_LendCpuMask(&mask);
+                // update num threads
+                cpu_set_t active_mask;
+                CPU_XOR(&active_mask, &process_mask, &mask);
+                CPU_AND(&active_mask, &process_mask, &active_mask);
+                omp_set_num_threads(CPU_COUNT(&active_mask));
+            } else if (lewi_reclaim) {
+                printf("\nReclaiming\n");
+                DLB_Reclaim();
+                omp_set_num_threads(CPU_COUNT(&process_mask));
+            }
+
+            // reset flags
+            lewi_lend = false;
+            lewi_reclaim = false;
         }
 
         /* Obtain CPU binding of all threads */
