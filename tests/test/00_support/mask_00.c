daa9088a1d2c546e4577b2016b83d3936ba7597e
Victor Lopez <victor.lopez@bsc.es>
2025-02-28 16:29:32 +0100

    Fix test
    CPU_SETSIZE is machine-dependent

--- tests/test/00_support/mask_00.c
+++ tests/test/00_support/mask_00.c
@@ -28,6 +28,7 @@
 
 #include <assert.h>
 #include <sched.h>
+#include <stdio.h>
 #include <string.h>
 
 int main(int argc, char *argv[]) {
@@ -113,7 +114,7 @@ int main(int argc, char *argv[]) {
     assert( mu_count(&result) == CPU_COUNT(&mask1) );
 
     // warning: from this point mask0 is no longer an empty mask
-    cpu_set_t fl_mask, full_mask, empty_mask, mask5, mask200;
+    cpu_set_t empty_mask, mask5, mask200;
     CPU_ZERO(&empty_mask);
     CPU_ZERO(&mask0);
     CPU_ZERO(&mask5);
@@ -121,8 +122,30 @@ int main(int argc, char *argv[]) {
     CPU_SET(0, &mask0);
     CPU_SET(5, &mask5);
     CPU_SET(200, &mask200);
-    mu_parse_mask("0,1023", &fl_mask);
-    mu_parse_mask("0-1023", &full_mask);
+
+    /* CPU_SETSIZE is machine-dependent */
+    enum { BUFFER_SIZE = 16 };
+    int max_cpuid = CPU_SETSIZE - 1;
+
+    /* first and last, on most systems: 0,1023 */
+    char fl_mask_raw[BUFFER_SIZE];
+    char fl_mask_sq[BUFFER_SIZE];
+    char fl_mask_quoted[BUFFER_SIZE];
+    snprintf(fl_mask_raw, BUFFER_SIZE, "0,%d", max_cpuid);
+    snprintf(fl_mask_sq, BUFFER_SIZE, "[0,%d]", max_cpuid);
+    snprintf(fl_mask_quoted, BUFFER_SIZE, "\"0,%d\"", max_cpuid);
+    cpu_set_t fl_mask;
+    mu_parse_mask(fl_mask_raw, &fl_mask);
+
+    /* first to last, on most systems: 0-1023 */
+    char full_mask_raw[BUFFER_SIZE];
+    char full_mask_sq[BUFFER_SIZE];
+    char full_mask_quoted[BUFFER_SIZE];
+    snprintf(full_mask_raw, BUFFER_SIZE, "0-%d", max_cpuid);
+    snprintf(full_mask_sq, BUFFER_SIZE, "[0-%d]", max_cpuid);
+    snprintf(full_mask_quoted, BUFFER_SIZE, "\"0-%d\"", max_cpuid);
+    cpu_set_t full_mask;
+    mu_parse_mask(full_mask_raw, &full_mask);
 
     /* mu_get_single_cpu */
     assert( mu_get_single_cpu(&mask0) == 0);
@@ -142,7 +165,7 @@ int main(int argc, char *argv[]) {
     assert( mu_get_last_cpu(&mask0) == 0 );
     assert( mu_get_last_cpu(&mask1) == 1 );
     assert( mu_get_last_cpu(&mask200) == 200 );
-    assert( mu_get_last_cpu(&full_mask) == 1023);
+    assert( mu_get_last_cpu(&full_mask) == max_cpuid);
     assert( mu_get_last_cpu(&empty_mask) == -1 );
 
     /* mu_get_next_cpu */
@@ -158,10 +181,10 @@ int main(int argc, char *argv[]) {
     assert( mu_get_next_cpu(&full_mask, 62) == 64 );
     assert( mu_get_next_cpu(&full_mask, 63) == 64 );
     CPU_SET(63, &full_mask);
-    assert( mu_get_next_cpu(&fl_mask, 0) == 1023 );    /* 1st ulong */
-    assert( mu_get_next_cpu(&fl_mask, 65) == 1023 );   /* 2nd ulong */
-    assert( mu_get_next_cpu(&fl_mask, 130) == 1023 );  /* 3rd ulong */
-    assert( mu_get_next_cpu(&fl_mask, 250) == 1023 );  /* 4th ulong */
+    assert( mu_get_next_cpu(&fl_mask, 0) == max_cpuid );    /* 1st ulong */
+    assert( mu_get_next_cpu(&fl_mask, 65) == max_cpuid );   /* 2nd ulong */
+    assert( mu_get_next_cpu(&fl_mask, 130) == max_cpuid );  /* 3rd ulong */
+    assert( mu_get_next_cpu(&fl_mask, 250) == max_cpuid );  /* 4th ulong */
 
     /* first, last and next */
     int sum = 0;
@@ -185,8 +208,8 @@ int main(int argc, char *argv[]) {
     assert( strcmp(mu_to_str(&mask2), "[0-3]") == 0 );
     assert( strcmp(mu_to_str(&mask5), "[5]") == 0 );
     assert( strcmp(mu_to_str(&empty_mask), "[]") == 0 );
-    assert( strcmp(mu_to_str(&fl_mask), "[0,1023]") == 0 );
-    assert( strcmp(mu_to_str(&full_mask), "[0-1023]") == 0 );
+    assert( strcmp(mu_to_str(&fl_mask), fl_mask_sq) == 0 );
+    assert( strcmp(mu_to_str(&full_mask), full_mask_sq) == 0 );
 
     /* mu_get_quoted_mask */
     enum { BUFFER_LEN = 16 };
@@ -200,9 +223,9 @@ int main(int argc, char *argv[]) {
     mu_get_quoted_mask(&empty_mask, buffer, BUFFER_LEN);
     assert( strcmp(buffer, "\"\"") == 0 );
     mu_get_quoted_mask(&fl_mask, buffer, BUFFER_LEN);
-    assert( strcmp(buffer, "\"0,1023\"") == 0 );
+    assert( strcmp(buffer, fl_mask_quoted) == 0 );
     mu_get_quoted_mask(&full_mask, buffer, BUFFER_LEN);
-    assert( strcmp(buffer, "\"0-1023\"") == 0 );
+    assert( strcmp(buffer, full_mask_quoted) == 0 );
 
     /* None of the previous tests should initialize mask utils */
     assert( mu_testing_is_initialized() == false );
